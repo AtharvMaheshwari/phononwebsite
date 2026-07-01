cf0917cb4090fef16b5305229a68e9a03e392e08
AtharvMaheshwari <atharvmaheshwari7@gmail.com>
2026-07-01 22:09:06 +0200

    Changed defualt material to test Al4Li4O8

--- src/main.js
+++ src/main.js
@@ -62,7 +62,7 @@ p.setExportXSFButton($('#xsf'));
 p.setTitle($('#name'));
 
 p.updateMenu();
-p.getUrlVars({json: "data/localdb/graphene/data.json", name:"Graphene [1]"});
+p.getUrlVars({json: "data/localdb2/al4li4o8/data.json", name:"Al4Li4O8 ( test )"}); //default material
 
 //set dom objects vibcrystal
 v.setCameraDirectionButton($('#camerax'),'x');
